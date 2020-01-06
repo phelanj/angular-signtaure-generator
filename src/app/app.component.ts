744ac2b4e4f11b619063886ca2f675bcf751873f
Jack Phelan <jack@modalight.com>
2020-01-06 15:37:34 -0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { SignatureComponent } from './signature/signature.component';
 import { Component } from '@angular/core';
+import { version } from '../../package.json';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  version: string = version;
   job: string;
   name: string;
   title = 'Signature Generator';
