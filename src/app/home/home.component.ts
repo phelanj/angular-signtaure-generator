af99c235a33f3b35a3330c708998dfa88f8432a4
Jack Phelan <jack@modalight.com>
2020-01-10 13:35:36 -0800

    going through changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { version } from '../../../package.json'
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,8 +8,9 @@ import { version } from '../../../package.json'
 })
 export class HomeComponent implements OnInit {
   version:string = version;
-  constructor() { }
-
+  constructor(private http: HttpClient) { }
+// this.http.get('http://localhost:port/team-members.json')
+//     .subscribe(data => console.log(data));
   ngOnInit() {
   }
 
